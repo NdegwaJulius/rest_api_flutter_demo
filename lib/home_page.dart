60a95602f7adcb21f7efc9980eba85eaf5c71245
NdegwaJulius <juliusndegwa2@gmail.com>
2023-02-07 21:31:48 +0300

    fetch data from API

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/post.dart';
+
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
@@ -8,11 +10,33 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List<Post>? posts;
+  var isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //fetch data from API
+  }
+
+  getData() async {
+    //posts = await
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('POSTS'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Text('Hi Julius'),
+          );
+        },
       ),
     );
   }
