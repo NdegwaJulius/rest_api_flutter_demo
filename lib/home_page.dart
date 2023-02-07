513ee9b6a88479dc3c3656325619cc1606ae739e
NdegwaJulius <juliusndegwa2@gmail.com>
2023-02-07 22:09:09 +0300

    complete data fetch  from API

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api_flutter/services/remote_services.dart';
 
 import 'models/post.dart';
 
@@ -11,17 +12,22 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   List<Post>? posts;
-  var isLoading = false;
+  var isLoaded = false;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     //fetch data from API
+    getData();
   }
 
   getData() async {
-    //posts = await
+    posts = await RemoteClass().getPosts();
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
   }
 
   @override
@@ -30,13 +36,53 @@ class _HomepageState extends State<Homepage> {
       appBar: AppBar(
         title: Text('POSTS'),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Container(
-            child: Text('Hi Julius'),
-          );
-        },
+      body: Visibility(
+        visible: isLoaded,
+        child: ListView.builder(
+          itemCount: posts?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          posts![index].title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          posts![index].body ?? '',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
       ),
     );
   }
